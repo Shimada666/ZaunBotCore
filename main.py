fce1d55ac18d1595faaa979f13cdd8b29cf6b40c
Shimada666 <649940882@qq.com>
2021-08-10 14:30:31 +0800

    feat: spec words

--- main.py
+++ main.py
@@ -13,8 +13,12 @@ class Corpus:
 class ZaunBot:
     def __init__(self):
         with open('data.json', 'r', encoding='utf8') as f:
-            data = f.read()
-            self.corpora = json.loads(data)
+            data = json.loads(f.read())
+            self.spec_keywords = set()
+            self.base_keywords = data['baseKeywords']
+            self.corpora = data['corpus']
+            for corpus in self.corpora:
+                self.spec_keywords |= set(corpus['keywords'])
 
     def list_generic_answer(self) -> typing.List[str]:
         """
@@ -32,15 +36,33 @@ class ZaunBot:
         answers = self.list_generic_answer()
         return random.choice(answers)
 
+    def words_in_text(self, words: typing.Iterable[str], text: str) -> bool:
+        """
+        关键词是否包含在语句里
+        :param words:
+        :param text:
+        :return:
+        """
+        for word in words:
+            if word in text:
+                return True
+        return False
+
     def match_text(self, text: str):
         """
         输入骂人的话，匹配最佳回骂
+        1. 包含具体关键词，优先返回具体回骂
+        2. 包含基本关键词，返回通用回骂
         @param text:
         """
-        pass
+        # if self.words_in_text(self.base_keywords, text):
+
+        return self.get_one_generic_answer()
 
 
 if __name__ == '__main__':
     print(ZaunBot().corpora)
+    print(ZaunBot().base_keywords)
+    print(ZaunBot().spec_keywords)
     print(ZaunBot().list_generic_answer())
     print(ZaunBot().get_one_generic_answer())
